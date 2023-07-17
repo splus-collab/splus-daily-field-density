f528eda6272f410c314f27cfb0ab9d2d6335b116
herpichfr <fabiorafaelh@gmail.com>
2023-07-17 15:28:48 +0100

    Add docstring and document code

--- calc_fields_density.py
+++ calc_fields_density.py
@@ -2,12 +2,17 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Apr  6 11:00:00 2020
-Last modified on Mon Apr  6 11:00:00 2020
+Last modified on Mon Jul 17 2023
 Author: Fabio R Herpich
 Github: https://github.com/splus-collab/splus-daily-field-density
 Description: Calculate the density of fields for a
 given night within a user-defined period of time.
 """
+
+__author__ = 'Fabio R Herpich'
+__version__ = '{tag}'
+__date__ = '2023-07-17'
+__email__ = 'fabio.herpich@ast.cam.ac.uk'
 
 from __future__ import print_function
 import numpy as np
@@ -24,7 +29,6 @@ import sys
 import argparse
 import multiprocessing as mp
 import logging
-# import warnings
 import colorlog
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -64,6 +68,11 @@ def parser():
     # add argument to require sym_file if --oper_type=plot
     parser.add_argument('-s', '--sym_file', help='Ouput file from symulation',
                         required=False)
+    # add argument to receive the date of table last update
+    parser.add_argument('-u', '--update_date',
+                        help='Date of S-PLUS observations last update.\
+                        Format YYYY-MM-DD (default: today)',
+                        required=False, type=str)
 
     args = parser.parse_args()
 
@@ -73,6 +82,7 @@ def parser():
 # use a function to restart the logger before every run
 
 def call_logger():
+    """Configure the logger."""
     # reset logging config
     logging.shutdown()
     logging.root.handlers.clear()
@@ -103,8 +113,22 @@ def call_logger():
 # Calculate the if a field is observable for a given night
 def calc_field_track(f, night_starts, tab_name):
     """
-    Calculate the if a field is observable for a given night.
+    Calculate the observability of a field for a given night.
+    Parameters:
+    - f (astropy.table.Table): Table containing field information.
+    - night_starts (str): Start date of the night in the format 'YYYY-MM-DD'.
+    - tab_name (str): Name of the output table.
+    Returns:
+    None
+    This function calculates the observability of each field in the provided table for a given night.
+    It determines if a field is observable based on various conditions such as the separation from the moon,
+    moon brightness, and altitude of the field during the night. The results are saved in a CSV table.
+    Note: The site is hardcoded to CTIO (Cerro Tololo Inter-American Observatory).
+    Saves a CSV table with the following columns:
+    - field_name (str): Field name.
+    - observable (bool): True if the field is observable, False otherwise.
     """
+
     call_logger()
     logger = logging.getLogger(__name__)
 
@@ -154,10 +178,6 @@ def calc_field_track(f, night_starts, tab_name):
                 separation: ' + str(moon_pos.separation(mycoords).value))
         elif (moon_brightness >= .85):
             if (f['NAME'][i].split('-')[-1][0] not in ['b', 'd']):
-                # myaltaz = mycoords.transform_to(
-                #     AltAz(obstime=set_time, location=mysite))
-
-                # myaltazs_tonight = mycoords.transform_to(frame_tonight)
 
                 myaltazs_time_overnight = mycoords.transform_to(
                     frame_time_overnight)
@@ -191,10 +211,6 @@ def calc_field_track(f, night_starts, tab_name):
                     moon', moon_brightness, 'sep:\
                     ', moon_pos.separation(mycoords).value)
         else:
-            # myaltaz = mycoords.transform_to(
-            #     AltAz(obstime=set_time, location=mysite))
-            #
-            # myaltazs_tonight = mycoords.transform_to(frame_tonight)
 
             myaltazs_time_overnight = mycoords.transform_to(
                 frame_time_overnight)
@@ -234,6 +250,11 @@ def calc_field_track(f, night_starts, tab_name):
 
 
 def generate_date_range(start_date, end_date):
+    """
+    Generate a list of dates between a start and end date.
+    start_date and end_date must be datetime objects.
+    Returns a list of dates in the format YYYY-MM-DD.
+    """
     date_range = [start_date.strftime('%Y-%m-%d')]
     current_date = start_date
 
@@ -245,7 +266,15 @@ def generate_date_range(start_date, end_date):
 
 
 def run_calc_field_track(workdir, f, night_range):
-    """Run the calc_field_track function for a range of nights."""
+    """
+    Run the calc_field_track function for a range of nights.
+    workdir: path to the working directory
+    f: table of the fields
+    night_range: list of nights to calculate
+    Returns: None
+    This function will create a table for each night in the night_range
+    and save it in the workdir/outputs directory.
+    """
 
     call_logger()
     logger = logging.getLogger(__name__)
@@ -268,7 +297,15 @@ def run_calc_field_track(workdir, f, night_range):
 
 
 def stack_tables(workdir, night_range, path_to_final_output):
-    """Stack the tables for the different nights."""
+    """
+    Stack the tables for the different nights.
+    workdir: path to the working directory
+    night_range: list of nights to calculate
+    path_to_final_output: path to the final output file
+    Returns: None
+    This function will create a CSV table with all the nights in the
+    night_range and save it in the path_to_final_output directory.
+    """
 
     call_logger()
     logger = logging.getLogger(__name__)
@@ -300,7 +337,22 @@ def stack_tables(workdir, night_range, path_to_final_output):
 
 
 def main_sym(args):
-    """Main function."""
+    """
+    Main function to calculate the field track. This function will
+    calculate the field track for a range of nights and save the
+    results in a CSV table. It will itereate over the nights and
+    calculate the field track for each night. Then it will stack the
+    tables for the different nights and save the final table.
+    The iteration is done in parallel using the multiprocessing
+    module. The number of cores to use can be specified with the
+    --ncores argument. The default is to use 1 core.
+
+    Args:
+        args: arguments from the command line
+
+    Returns:
+        None
+    """
     call_logger()
     logger = logging.getLogger(__name__)
 
@@ -408,8 +460,28 @@ def check_dates(workdir, fname, start_date, end_date):
         raise ValueError('End date not in the input file')
 
 
-def plot_density(workdir, finput, ffoot, start_date, end_date, output, verbose, save):
-    """Plot the tile density using the input file"""
+def plot_density(workdir, finput, ffoot, start_date, end_date, output,
+                 last_update=None, verbose=False, save=False):
+    """
+    Plot the tile density using the input file.
+
+    Args:
+        workdir (str): The working directory.
+        finput (str): The input file.
+        ffoot (str): The footprint file.
+        start_date (str): The start date.
+        end_date (str): The end date.
+        output (str): The output file name.
+        last_update (str): The last update date.
+        verbose (bool): Whether to print status messages.
+        save (bool): Whether to save the plot.
+
+    Returns:
+        None
+    """
+    # define last date of update
+    last_update = datetime.datetime.now().strftime(
+        "%Y/%m/%d") if last_update is None else last_update
     # read the input file and the footprint file
     fi = ascii.read(os.path.join(workdir, finput))
     foot = ascii.read(os.path.join(workdir, ffoot))
@@ -462,8 +534,7 @@ def plot_density(workdir, finput, ffoot, start_date, end_date, output, verbose, 
     plt.gca().invert_yaxis()
     plt.grid()
     plt.title('$\mathrm{Total\ of\ tiles\ remaining\ as\ of\ %s:\ %i}$' %
-              (datetime.datetime.now().strftime("%Y/%m/%d"),
-               foot['NAME'][~mask].size), fontsize=16)
+              (last_update, foot['NAME'][mask].size), fontsize=16)
 
     plt.tight_layout()
 
@@ -486,7 +557,9 @@ def plot_density(workdir, finput, ffoot, start_date, end_date, output, verbose, 
 
 
 def main_plot(args):
-    """Main function"""
+    """
+    Main function to plot the tile density using the input file.
+    """
     # get the arguments
     finput = args.sym_file
     ffoot = args.fields
@@ -496,13 +569,14 @@ def main_plot(args):
     verbose = args.verbose
     save = args.save_plot
     workdir = args.workdir
+    last_update = args.last_update
 
     # check if start_date and end_date are within the input file
     check_dates(workdir, finput, start_date, end_date)
 
     # plot the tile density using the input file
     plot_density(workdir, finput, ffoot, start_date,
-                 end_date, output, verbose, save)
+                 end_date, output, verbose=verbose, save=save, last_update=last_update)
 
 
 if __name__ == '__main__':
